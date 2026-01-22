c1db970ecbd601c3e256bb8acb9cb62e843401af
Tanish Rajput <tanishrajput9@gmail.com>
2026-01-22 22:57:20 +0530

    updated risk assesment import

--- tools/mutate_tools.py
+++ tools/mutate_tools.py
@@ -13,7 +13,7 @@ from models.tool_results import ToolResult
 
 # Week 2 Security Imports
 from core.security import path_validator, security_enforcer
-from core.risk_assessment import risk_assessor
+from core.risk_assesment import risk_assessor
 from core.confirmation import ConfirmationManager
 from core.backup_manager import backup_manager
 from core.exceptions import PathSecurityError, ValidationError
